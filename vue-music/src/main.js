3ae0dc4ed500b664130c61b654ac7c3f8c65b55a
lyc <269632358@qq.com>
2017-10-23 17:26:15 +0800

    页面骨架搭建完成

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,12 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
-
+import fastclick from 'fastclick'
+// Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 /* eslint-disable no-new */
+fastclick.attach(document.body)
 new Vue({
   el: '#app',
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
