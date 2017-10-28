867793975cf3491944238f23397a2764d7d47d82
lyc <269632358@qq.com>
2017-10-28 17:51:11 +0800

    111

--- vue-music/src/common/js/jsonp.js
+++ vue-music/src/common/js/jsonp.js
@@ -1,21 +1,21 @@
 import originJsonp from 'jsonp'
-export default function jsonp(url, data, option) {
-    url += (url.indexOf('?') < 0 ? '?': '&') + param(data)
-    return new Promise((resolve, reject) => {
-        originJsonp(url, option,(err,data) => {
-            if (!err) {
-                resolve(data)
-            } else {
-                reject(err)
-            }
-        })
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
     })
+  })
 }
 function param (data) {
-    let url = ''
-    for(let i in data) {
-        let value = data[i] !== undefined?data[i]: ''
-        url += `${i}=${encodeURLComponent(value)}`
-    }
-    return url ? url.substring(1) : ''
+  let url = ''
+  for (let i in data) {
+    let value = data[i] !== undefined ? data[i] : ''
+    url += '&' + i + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
 }
