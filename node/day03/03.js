4b27518d45941f75b19e11ebef09aa65784c86b7
lyc081811 <269632358@qq.com>
2017-11-21 22:13:13 +0800

    看了一会

--- node/day03/03.js
+++ node/day03/03.js
@@ -1,1 +1,12 @@
-// 如果fs.readfile(path, option, csllback)中option没传的时候会得到一个buffer(缓冲区)
+const fs = require('fs');
+const path = require('path');
+const iconv = require('iconv-lite');
+
+fs.readFile(path.join(__dirname, '血染的风采.lrc'),(error,data) => {
+	var lines = iconv.decode(data,'gbk').split('\n');
+	// console.log(lines.length);
+	lines.forEach(line => {
+		var reg = /\[(\d{2})\:(\d{2})\.(\d{2})\]\s(.+)/;
+	})
+});
+
