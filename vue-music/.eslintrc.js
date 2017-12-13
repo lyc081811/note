2bead30d29ad9f07ae6c3441355d3ee34956ed1d
lyc081811 <269632358@qq.com>
2017-12-13 22:19:37 +0800

    343

--- vue-music/.eslintrc.js
+++ vue-music/.eslintrc.js
@@ -18,8 +18,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
+    'arrow-parens': 0,
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'eol-last': 0,
+    'space-before-function-paren': 0
   }
 }
