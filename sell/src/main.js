0f86193d9b224523b39d58a543ad1b1d45337721
lyc <269632358@qq.com>
2017-12-26 16:57:02 +0800

    提交(sell)

--- sell/src/main.js
+++ sell/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from 'components/goods/goods';
 import seller from 'components/seller/seller';
 import ratings from 'components/ratings/ratings';
 
+import 'common/stylus/index.styl';
+
 Vue.use(VueRouter);
+Vue.use(VueResource);
 /* eslint-disable no-new */
 // new Vue({
 //   el: 'body',
@@ -14,7 +18,8 @@ Vue.use(VueRouter);
 
 let app = Vue.extend(App);
 let router = new VueRouter({
-  linkActiveClass: 'active'
+  linkActiveClass: 'active',
+  transitionOnLoad: true
 });
 router.map({
   '/goods': {
