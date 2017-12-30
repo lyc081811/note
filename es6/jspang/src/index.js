cae4eb0418f82de01548a9824f1ac8c73a4afeb2
lyc081811 <269632358@qq.com>
2017-12-30 21:25:52 +0800

    提交jspang的es6

---
+++ es6/jspang/src/index.js
@@ -0,0 +1,21 @@
+// var a = 'jspang';
+// // console.log(a);
+// // window.onload = function () {
+// // 	console.log(a);
+// // } 
+
+// {
+// 	let a = '技术胖';
+// }
+// console.log(a);
+
+
+// for (var i =  0; i < 10; i++) {
+// 	console.log('循环体内' + i);
+// }
+	
+// 	console.log('循环体外' + i)
+
+let foo;
+({foo} = {foo: "jspang"});
+console.log(foo);
