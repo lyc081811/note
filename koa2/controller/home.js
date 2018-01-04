faee1055159b30a9119ecec7696ab734219ab0d4
lyc <269632358@qq.com>
2018-01-04 18:16:16 +0800

    koa2/app.js

--- koa2/controller/home.js
+++ koa2/controller/home.js
@@ -5,12 +5,10 @@ module.exports = {
         await ctx.render("home/index", { title: "iKcamp欢迎您" })
     },
     home: async (ctx, next) => {
-        console.log(ctx.request.query)
-        console.log(ctx.request.querystring)
-        ctx.response.body = '<h1>HOME page</h1>'
+        ctx.send({status: '200'})
+        // ctx.response.body = '<h1>HOME page</h1>'
     },
     homeParams: async (ctx, next) => {
-        console.log(ctx.params)
         ctx.response.body = '<h1>HOME page /:id/:name</h1>'
     },
     login: async (ctx, next) => {
