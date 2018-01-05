8a6a1644b1d57d51bcb463991b46da80caa9d7f2
lyc <269632358@qq.com>
2018-01-05 18:09:31 +0800

    提交一下.周末继续

---
+++ es6/jspang/src/symbol.js
@@ -0,0 +1,7 @@
+var a = new String;
+var b = new Number;
+var c = new Boolean;
+var d = new Array;
+var e = new Object;
+var f = Symbol();
+console.log(typeof (d));
--- koa2/middleware/mi-log/logger.js
+++ koa2/middleware/mi-log/logger.js
@@ -1,15 +1,40 @@
 const log4js = require('log4js');
-module.exports = (options) => {
+const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
+
+module.exports = () => {
+    const contextLogger = {}
+    const config = {
+        appenders: {
+            cheese: {
+                type: 'dateFile', // 日志类型 
+                filename: `logs/task`,  // 输出的文件名
+                pattern: '-yyyy-MM-dd.log',  // 文件名增加后缀
+                alwaysIncludePattern: true   // 是否总是有后缀名
+            }
+        },
+        categories: {
+            default: {
+                appenders: ['cheese'],
+                level: 'info'
+            }
+        }
+    }
+
+    const logger = log4js.getLogger('cheese');
+
     return async (ctx, next) => {
         const start = Date.now()
-        log4js.configure({
-            appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-            categories: { default: { appenders: ['cheese'], level: 'info' } }
-        });
-        const logger = log4js.getLogger('cheese');
+
+        log4js.configure(config)
+        methods.forEach((method, i) => {
+            contextLogger[method] = (message) => {
+                logger[method](message)
+            }
+        })
+        ctx.log = contextLogger;
+
         await next()
-        const end = Date.now()
-        const responseTime = end - start;
+        const responseTime = Date.now() - start;
         logger.info(`响应时间为${responseTime / 1000}s`);
     }
 }
