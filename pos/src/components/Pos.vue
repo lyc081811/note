aabba9d930eef9ca09e602ae116a35983965761b
lyc081811 <269632358@qq.com>
2018-01-11 19:23:29 +0800

    123

--- pos/src/components/Pos.vue
+++ pos/src/components/Pos.vue
@@ -1,15 +1,75 @@
 <template>
   <div class="pos">
-    hello pos demo
+    <el-row>
+      <el-col :span="7" class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableDate" border show-summary style="width: 100%">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="量" width="50"></el-table-column>
+              <el-table-column prop="price" label="金额" width="70"></el-table-column>
+              <el-table-column label="操作" width="100" fixed="right">
+                <template scope="scope">
+                  <el-button type="text" size="small">删除</el-button>
+                  <el-button type="text" size="small">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">
+挂单
+          </el-tab-pane>
+          <el-tab-pane label="外卖">
+外卖
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="17">
+        我是产品栏
+      </el-col>
+    </el-row>
   </div>
 </template>
- <script>
- export default {
-   name: 'pos'
- }
- </script>
+<script>
+export default {
+  name: 'pos',
+  data () {
+    return {
+      tableDate: [
+        {
+          goodsName: '可口可乐',
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: '香辣鸡腿堡',
+          price: 15,
+          count: 1
+        },
+        {
+          goodsName: '爱心薯条',
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: '甜筒',
+          price: 8,
+          count: 1
+        }
+      ]
+    }
+  },
+  mounted () {
+    var orderHeight = document.body.clientHeight
+    document.getElementById('order-list').style.height = orderHeight + 'px'
+  }
+}
+</script>
 
- <style scoped>
-
- </style>
+<style scoped>
+.pos-order {
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+}
+</style>
 
