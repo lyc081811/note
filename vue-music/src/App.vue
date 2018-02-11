2e56d7d8751ea001723fcc6d13c9fc55bd8847da
lyc <269632358@qq.com>
2018-02-11 15:27:07 +0800

    犬瘟热

---
+++ vue-music/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
