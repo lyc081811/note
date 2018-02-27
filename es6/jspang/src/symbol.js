b9090fe35dba9c9155b06d29c3072514ad5ad6fb
lyc <269632358@qq.com>
2018-02-27 11:34:03 +0800

    es6实战项目目录构建

--- es6/jspang/src/symbol.js
+++
@@ -1,7 +0,0 @@
-var a = new String;
-var b = new Number;
-var c = new Boolean;
-var d = new Array;
-var e = new Object;
-var f = Symbol();
-console.log(typeof (d));
--- webpack-demo/src/entry.js
+++
@@ -1,3 +0,0 @@
-import css from './css/index.css';
-import less from './css/a.less';
-document.getElementById("title").innerHTML = "Hello Webpack";
