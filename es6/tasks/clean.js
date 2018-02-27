aacd6c8f194bcba382037fbeb6072c9a4dc787a8
lyc <269632358@qq.com>
2018-02-27 16:18:58 +0800

    修改

--- es6/tasks/clean.js
+++ es6/tasks/clean.js
@@ -2,6 +2,6 @@ import gulp from 'gulp';
 import del from 'del';
 import args from './util/args';
 
-gulp.watch('clean', () => {
-	return del(['server/public', 'server/views'])
+gulp.task('clean',()=>{
+  return del(['server/public','server/views'])
 })
