599b9c17e521132594dbdf7cd4e5fa31b6da61f7
lyc081811 <269632358@qq.com>
2018-02-28 00:12:12 +0800

    添加解构

---
+++ es6/app/js/class/lesson1.js
@@ -0,0 +1,81 @@
+// 解构
+
+{
+	let a, b, resr;
+	[a, b] = [1, 2];
+	console.log(a, b);
+}
+{
+	let a, b, rest;
+	[a, b, ...rest] = [1,2,3,4,5,6];
+	console.log(a,b,rest);
+}
+{
+	let a, b;
+	({a,b} = {a:1, b:2})
+	console.log(a, b);
+}
+
+// arrary的解构
+{
+	let a, b, c, resr;
+	[a, b, c = 3] = [1, 2];
+	console.log(a, b, c);
+}
+
+{
+	// 变量交换
+	let a = 1;
+	let b = 2;
+	[a,b] = [b,a];
+	console.log(a,b);
+}
+
+{
+	function f() {
+		return [1,2];
+	}
+	let a,b;
+	[a,b] = f();
+	console.log(a,b)
+}
+
+{
+	function f() {
+		return [1, 2, 3, 4, 5];
+	}
+	let a, b, c;
+	[a, , , b] = f();
+	console.log(a,b)
+}
+
+{
+	function f() {
+		return [1, 2, 3, 4, 5];
+	}
+	let a, b, c;
+	[a, ...b] = f();
+	console.log(a,b);
+}
+
+// object的解构
+{
+	let o = {p:42,q: true};
+	let {p,q} = o;
+	console.log(p,q)
+}
+{
+	let {a = 10, b = 5} = {b : 2};
+	console.log(a,b);
+}
+{
+	let data = {
+		title: 'abc',
+		test: [{
+			title: '123',
+			desc: '567'
+		}]
+	}
+	let {title:atitle, test: [{title: btitle}]} = data;
+	console.log(atitle, btitle);
+}
---
+++ es6/server/public/js/index.js
@@ -0,0 +1,215 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	__webpack_require__(2);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+	function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
+	// 解构
+
+	{
+		var a = void 0,
+		    b = void 0,
+		    resr = void 0;
+		a = 1;
+		b = 2;
+
+		console.log(a, b);
+	}
+	{
+		var _a = void 0,
+		    _b = void 0,
+		    rest = void 0;
+		_a = 1;
+		_b = 2;
+		rest = [3, 4, 5, 6];
+
+		console.log(_a, _b, rest);
+	}
+	{
+		var _a2 = void 0,
+		    _b2 = void 0;
+		var _a$b = { a: 1, b: 2 };
+		_a2 = _a$b.a;
+		_b2 = _a$b.b;
+
+		console.log(_a2, _b2);
+	}
+
+	// arrary的解构
+	{
+		var _a3 = void 0,
+		    _b3 = void 0,
+		    c = void 0,
+		    _resr = void 0;
+		var _ref = [1, 2];
+		_a3 = _ref[0];
+		_b3 = _ref[1];
+		var _ref$ = _ref[2];
+		c = _ref$ === undefined ? 3 : _ref$;
+
+		console.log(_a3, _b3, c);
+	}
+
+	{
+		// 变量交换
+		var _a4 = 1;
+		var _b4 = 2;
+		var _ref2 = [_b4, _a4];
+		_a4 = _ref2[0];
+		_b4 = _ref2[1];
+
+		console.log(_a4, _b4);
+	}
+
+	{
+		var f = function f() {
+			return [1, 2];
+		};
+
+		var _a5 = void 0,
+		    _b5 = void 0;
+
+		var _f = f();
+
+		var _f2 = _slicedToArray(_f, 2);
+
+		_a5 = _f2[0];
+		_b5 = _f2[1];
+
+		console.log(_a5, _b5);
+	}
+
+	{
+		var _f3 = function _f3() {
+			return [1, 2, 3, 4, 5];
+		};
+
+		var _a6 = void 0,
+		    _b6 = void 0,
+		    _c = void 0;
+
+		var _f4 = _f3();
+
+		var _f5 = _slicedToArray(_f4, 4);
+
+		_a6 = _f5[0];
+		_b6 = _f5[3];
+
+		console.log(_a6, _b6);
+	}
+
+	{
+		var _f6 = function _f6() {
+			return [1, 2, 3, 4, 5];
+		};
+
+		var _a7 = void 0,
+		    _b7 = void 0,
+		    _c2 = void 0;
+
+		var _f7 = _f6();
+
+		var _f8 = _toArray(_f7);
+
+		_a7 = _f8[0];
+		_b7 = _f8.slice(1);
+
+		console.log(_a7, _b7);
+	}
+
+	// object的解构
+	{
+		var o = { p: 42, q: true };
+		var p = o.p,
+		    q = o.q;
+
+		console.log(p, q);
+	}
+	{
+		var _b9 = { b: 2 },
+		    _b9$a = _b9.a,
+		    _a8 = _b9$a === undefined ? 10 : _b9$a,
+		    _b9$b = _b9.b,
+		    _b8 = _b9$b === undefined ? 5 : _b9$b;
+
+		console.log(_a8, _b8);
+	}
+	{
+		var data = {
+			title: 'abc',
+			test: [{
+				title: '123',
+				desc: '567'
+			}]
+		};
+
+		var atitle = data.title,
+		    _data$test = _slicedToArray(data.test, 1),
+		    btitle = _data$test[0].title;
+
+		console.log(atitle, btitle);
+	}
+
+/***/ })
+/******/ ]);
